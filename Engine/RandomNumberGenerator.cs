78bebc9160982ea3dc46d8225cdbf56a492dd4b4
Sarah Freitas <sarahjfreitas@gmail.com>
2019-07-07 07:19:47 -0300

    random number generator

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        static Random generator = new Random();
+
+        public static int NumberBetween(int min, int max)
+        {
+            return generator.Next(min, max + 1);
+        }
+    }
+}
