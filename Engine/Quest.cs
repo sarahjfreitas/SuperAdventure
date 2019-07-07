30369d450c72db01a69af912d1131830e4c77553
Sarah Freitas <sarahjfreitas@gmail.com>
2019-07-07 04:17:00 -0300

    add lista de itens em quest

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -14,6 +14,7 @@ namespace Engine
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
         public Item RewardItem { get; set; }
+        public List<QuestCompletionItem> GetQuestCompletionItems { get; set; }
         public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
             ID = id;
@@ -21,6 +22,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            GetQuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
 }
